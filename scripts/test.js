21f651df29e22c3bf29e5180a411bc3fd1c76c17
Faith <seventanyb@gmail.com>
2022-07-10 21:47:13 +0800

    fda

--- scripts/test.js
+++ scripts/test.js
@@ -1,6 +1,6 @@
 const { getInput }  = require('@actions/core')
 
 console.log('==============')
-console.log(getInput('test_token'))
+console.log(getInput('token'))
 console.log(getInput('other_token'))
 console.log('==============')
